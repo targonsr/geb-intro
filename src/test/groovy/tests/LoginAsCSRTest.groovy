200a528df2e6429ad27ccd42e842fa5ee42401ce
Ryszard Targoński <targonsr@gmail.com>
2019-03-06 15:13:46 +0100

    format single file

--- src/test/groovy/tests/LoginAsCSRTest.groovy
+++ src/test/groovy/tests/LoginAsCSRTest.groovy
@@ -3,7 +3,7 @@ package tests
 import geb.spock.GebReportingSpec
 import pages.HomePage
 
-class LoginAsCSRTest extends GebReportingSpec{
+class LoginAsCSRTest extends GebReportingSpec {
     def "Login as CSR test"() {
         given: "at Home Page"
         to HomePage
